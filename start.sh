7157ae6ccc1a7bf999acaa66bd778a86f9a5c6f8
Chans <imagechans@gmail.com>
2025-07-17 21:20:24 +0800

    1

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ if [ ! -n "$1" ] ;then
     echo "要修改的ip为空"
 else
     echo "ip为：$1"
-    sed -i "s/172.16.0.104/$1/g" docker-compose.yaml
+    sed -i "s/172.16.0.104/$1/g" docker-compose.yml
     sed -i "s/172.16.0.104/$1/g" nacos/env/nacos-standlone-mysql.env
     sed -i "s/172.16.0.104/$1/g" rocketMQ/data/broker/conf/broker.conf
     sed -i "s/172.16.0.104/$1/g" xxl-job/config/application.properties
